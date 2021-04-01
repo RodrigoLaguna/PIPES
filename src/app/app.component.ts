155766d715492ca1736d598c27d1ecc305202aa0
Rodrigo Laguna <rodrigolaguna90@gmail.com>
2021-04-01 02:11:35 -0600

    Uso de pipes final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre:string = 'Capitán América';
+
+  nombre2:string = 'rodrIgO lAgunA';
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI:number = Math.PI;
+
+  porcentaje:number = 0.234;
+
+  salario:number = 1234.5;
+
+  fecha:Date = new Date();
+
+  activar:boolean = true;
+
+  idioma:string = 'es';
+
+  videoUrl:string = 'https://www.youtube.com/embed/K6XsI0svtwE';
+
+  heroe = {
+    nombre:'Logan',
+    clave:'Wolverine',
+    edad:500,
+    direccion:{
+      calle:'Primera',
+      casa:20
+    }
+  };
+
+  valorPromesa = new Promise<string>( (resolve) =>{
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+  });
+
+  
+  cambiarIdioma(idioma:string){
+    if (idioma == 'en') {
+      this.idioma = 'en';
+    }else if(idioma == 'fr'){
+      this.idioma = 'fr';
+    }
+    else{
+      this.idioma = 'es';
+    }
+  }
+
 }
